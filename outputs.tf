5a6e5ecb8ff007d032962c4908c1aa84618c2020
Rick Moonen <rick@moonen.network>
2023-01-24 16:11:39 +0100

    fix: set correct outputs

--- outputs.tf
+++ outputs.tf
@@ -1,36 +1,16 @@
-output "lb_address_prefix" {
-  value = azurerm_subnet.vault_lb.address_prefixes[0]
-}
-
-output "lb_subnet_id" {
-  value = azurerm_subnet_network_security_group_association.vault_lb.id
-}
-
-output "vault_application_security_group_ids" {
-  value = [azurerm_application_security_group.vault.id]
-}
-
-output "vault_lb_network_security_group_name" {
-  value = azurerm_network_security_group.vault_lb.name
-}
-
-output "vault_network_security_group_name" {
-  value = azurerm_network_security_group.vault.name
-}
-
-output "vault_subnet_id" {
-  value = azurerm_subnet_network_security_group_association.vault.id
+output "lb_identity_id" {
+  value = azurerm_user_assigned_identity.load_balancer.id
 
   depends_on = [
-    azurerm_subnet_nat_gateway_association.vault,
+    azurerm_key_vault_access_policy.load_balancer_msi,
   ]
 }
 
-output "virtual_network_name" {
-  value = azurerm_virtual_network.vault.name
+output "vm_identity_id" {
+  value = azurerm_user_assigned_identity.vault.id
 
   depends_on = [
-    azurerm_subnet_network_security_group_association.vault,
-    azurerm_subnet_network_security_group_association.vault_lb,
+    azurerm_key_vault_access_policy.vault_msi,
+    azurerm_role_assignment.vault,
   ]
 }
